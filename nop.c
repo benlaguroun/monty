723afb82cc7306f8c928fd7a44c4abbf2d94f346
benlaguroun <benlaguroun@gmail.com>
2023-08-25 05:43:30 -0700

    mul

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_nop - Does nothing.
+ *
+ * @head: A pointer to the stack's head.
+ * @counter: The line number.
+ *
+ * Return: This function has no return value.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
