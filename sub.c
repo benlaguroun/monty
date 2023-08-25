3534015ad5871612f85bcec2901e0cdfe75aad72
benlaguroun <benlaguroun@gmail.com>
2023-08-25 06:10:54 -0700

    sub

---
+++ sub.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * f_sub - Performs subtraction between the top two elements of the stack.
+ *
+ * @head: A pointer to the stack's head.
+ * @counter: The line number.
+ *
+ * Return: This function has no return value.
+ */
+void f_sub(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
+}
